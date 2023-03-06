636b5dfa9ea3304bee39b93a9f5368f07f74d325
lpewton <liampewton99@gmail.com>
2023-03-06 10:50:39 +0000

    Add Ingredient model and migrate it to database

--- stock_controller_app/models.py
+++ stock_controller_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    price = models.FloatField()
+    image = CloudinaryField('image', default='placeholder')
+    units = models.IntegerField()
+
+    def __str__(self):
+        return self.name
